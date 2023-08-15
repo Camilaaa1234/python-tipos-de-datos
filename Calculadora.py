b80a13360b27156373873fd4622bafb4da5061c3
camila <mariac.morenoe@konradlorenz.edu.co>
2023-08-15 15:42:30 -0500

    Se creo modulo y potencia

--- Calculadora.py
+++ Calculadora.py
@@ -3,7 +3,7 @@ print("Ingrese el número 1")
 numero1 = int(input())
 print("Ingrese el número 2")
 numero2 = int(input())
-print("Ingrese la operación (+, -, *, /, %,**)")
+print("Ingrese la operación (+, -, *, /, %,#)")
 operacion = input()
 
 match operacion:
@@ -27,7 +27,7 @@ match operacion:
         # Operación división:
         modulo = numero1 % numero2
         print("El módulo es " + str(modulo))
-    case '**':
+    case '#':
         # Operación división:
         potencia = numero1**numero2
         print("La potencia es " + str(potencia))
